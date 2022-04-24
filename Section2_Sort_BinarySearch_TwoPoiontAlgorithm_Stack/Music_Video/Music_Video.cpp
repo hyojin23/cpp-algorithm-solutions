e3290b24332fce39d241fdbaa10c35f342a5b57f
hyojin <kimhj9292@gmail.com>
2022-04-24 23:42:36 +0900

    Study up to Music Video

---
+++ Section2_Sort_BinarySearch_TwoPoiontAlgorithm_Stack/Music_Video/Music_Video.cpp
@@ -0,0 +1,38 @@
+#include <cstdio>
+
+int a[1001], n;
+
+int Count(int s) {
+    int i, cnt = 1, sum = 0;
+    for (i = 1; i <= n; i++) {
+        if (sum + a[i] > s) {
+            cnt++;
+            sum = a[i];
+        } else
+            sum = sum + a[i];
+    }
+    return cnt;
+}
+
+int main() {
+//    freopen("input.txt", "rt", stdin);
+    int m, i, lt = 1, rt = 0, mid, res, max = -2147000000;
+    scanf("%d %d", &n, &m);
+    for (i = 1; i <= n; i++) {
+        scanf("%d", &a[i]);
+        rt = rt + a[i];
+        if (a[i] > max)
+            max = a[i];
+    }
+    while (lt <= rt) {
+        mid = (lt + rt) / 2;
+        if (mid >= max && Count(mid) <= m) {
+            res = mid;
+            rt = mid - 1;
+        } else {
+            lt = mid + 1;
+        }
+    }
+    printf("%d\n", res);
+    return 0;
+}
